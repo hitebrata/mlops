6298e13621f697764d29176f7d606f200d59632b
hitebrata <hitebrata.bhainsa@visnet.in>
2020-05-20 15:22:47 +0000

    sed commmit

--- replace.sh
+++ replace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i '21i model.add(Convolution2D(filters=32, kernel_size=(3,3), activation='relu',))' /mnt/d/kubernets/CNN_cat_and_dog_old.py
-sed -i '26i model.add(Dense(units=64, activation='relu'))' /mnt/d/kubernets/CNN_cat_and_dog_old.py
-sed -i 's/epochs=25/epochs=10/g' /mnt/d/kubernets/CNN_cat_and_dog_old.py
-sed -i 's/cnn_dataset/cnn_dataset_1/g' /mnt/d/kubernets/CNN_cat_and_dog_old.py
+sed -i '21i model.add(Convolution2D(filters=32, kernel_size=(3,3), activation='relu',))' /database/CNN_cat_and_dog_old.py
+sed -i '26i model.add(Dense(units=64, activation='relu'))' /database/CNN_cat_and_dog_old.py
+sed -i 's/epochs=25/epochs=10/g' /database/CNN_cat_and_dog_old.py
+sed -i 's/cnn_dataset/cnn_dataset_1/g' /database/CNN_cat_and_dog_old.py
