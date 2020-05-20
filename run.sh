09853523433446cff8bdd1093451fd71fb126354
hitebrata <hitebrata.bhainsa@visnet.in>
2020-05-20 19:09:00 +0000

    new line

--- run.sh
+++ run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 echo "preparing the CNN model"
-#nohup python3 -u ./CNN_cat_and_dog.py > output.log &
-python3 /home/CNN_cat_and_dog_old.py > /home/tensor.log 2>&1
+nohup python3 -u ./home/CNN_cat_and_dog_old.py > output.log
+
+
+
+echo "echecking the model accuracy"
+
+
+X=$(grep "val_accuracy" /home/output.log | awk '{print $NF}')
+echo "$X"
+Y=${X:2:2}
+echo "$Y"
+if [ "$Y <= 85" ]
+then
+http://35.193.7.112:8080/job/job3/build?token=1176263a616dcf06a723d56479a4e4034d
+else
+echo "try again"
+fi
