f24dde936cb9a02a25caec00cd95b31955225495
hitebrata <hitebrata.bhainsa@visnet.in>
2020-05-22 06:01:20 +0000

    replace dddcommit

--- CNN_cat_and_dog_old.py
+++ CNN_cat_and_dog_old.py
@@ -18,9 +18,13 @@ model.add(Convolution2D(filters=32,
                         activation='relu',
                        ))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(filters=32, kernel_size=(3,3), activation='relu',))
+model.add(Convolution2D(filters=32, kernel_size=(3,3), activation='relu',))
 model.summary()
 model.add(Flatten())
+model.add(Dense(units=64, activation='relu'))
 model.add(Dense(units=128, activation='relu'))
+model.add(Dense(units=64, activation='relu'))
 model.summary()
 model.add(Dense(units=1, activation='sigmoid'))
 model.summary()
@@ -33,12 +37,12 @@ train_datagen = ImageDataGenerator(
         horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
-        '/home/cnn_dataset_1/training_set/',
+        '/home/cnn_dataset_1_1/training_set/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 test_set = test_datagen.flow_from_directory(
-        '/home/cnn_dataset_1/test_set/',
+        '/home/cnn_dataset_1_1/test_set/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
@@ -52,7 +56,7 @@ model.save('cnn-cat-dog-model_old.h5')
 from keras.models import load_model
 m = load_model('cnn-cat-dog-model_old.h5')
 from keras.preprocessing import image
-test_image = image.load_img('/home/cnn_dataset_1/single_prediction/cat_or_dog_2.jpg', 
+test_image = image.load_img('/home/cnn_dataset_1_1/single_prediction/cat_or_dog_2.jpg', 
                target_size=(64,64))
 
 test_image = image.img_to_array(test_image)
--- replace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo sed -i '21i model.add(Convolution2D(filters=32, kernel_size=(3,3), activation='relu',))' /database/CNN_cat_and_dog_old.py
-sudo sed -i '26i model.add(Dense(units=64, activation='relu'))' /database/CNN_cat_and_dog_old.py
-sudo sed -i 's/epochs=25/epochs=10/g' /database/CNN_cat_and_dog_old.py
-sudo sed -i 's/cnn_dataset/cnn_dataset_1/g' /database/CNN_cat_and_dog_old.py
-sudo cd /database/; git init; git commit . -m "replace commit"; git push;
